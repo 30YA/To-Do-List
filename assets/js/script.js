595d72e325f78ae692aced9f1b269ce12f4635ec
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2021-12-11 20:56:26 +0330

    Finish add task & check task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,66 @@
 "use strict";
-// ------------------------------------------------
+// this function for add task : ------------------------------------------
+const addTask = document.querySelector(".add-item button");
+const todoContainer = document.querySelector(".todo-container ul");
+let inputValue = document.querySelector(".add-item input");
+let modalCounter = 0;
+addTask.addEventListener('click',addItem);
+
+function addItem(e) {
+    e.preventDefault();
+    if (inputValue.value != "") {
+        let li_tag = document.createElement("li");
+        li_tag.innerHTML = `<label>
+           <div class="clickable"></div>
+           <input type="checkbox">
+           <div class="custom-Checkbox"></div>
+           <h3 style="user-select: none;">${inputValue.value}</h3>
+           </label>
+           <div class="btns">
+           <button><i class="fas fa-edit"></i></button>
+           <button><i class="fas fa-times"></i></button>
+           </div>`;
+           todoContainer.append(li_tag);
+           inputValue.value = "";
+           document.querySelector(".modal").style.transform = "translate(400px,0)";
+           
+           
+        }else if(inputValue.value == ""){
+        modalCounter++;
+        document.querySelector(".modal").style.transform = "translate(0,0)";
+        if (modalCounter > 1) {
+            document.querySelector(".modal").classList = "modal animate__bounceIn";
+            setTimeout(() => {
+                document.querySelector(".modal").classList = "modal";
+            },900)
+        }
+        setTimeout(() => {
+            document.querySelector(".modal").style.transform = "translate(400px,0)";
+        },5000);
+    }
+}
+// this function for edite & Delete & checkbox Buttons : ------------------------------------------
+todoContainer.addEventListener('click',deleteLi);
+todoContainer.addEventListener('click',checkItem);
+function deleteLi(e) {
+    const classArray = [...e.target.classList];
+    if (classArray[1] == "fa-edit") {
+        console.log(e.target.parentElement.parentElement.parentElement);
+    }else if (classArray[1] == "fa-times"){
+        e.target.parentElement.parentElement.parentElement.remove();
+    }
+}
+function checkItem(e) {
+    let h3;
+    if (e.target.className == "clickable") {
+        h3 = e.target.parentElement.children[3];
+        h3.classList.toggle("complete");
+    }
+}
+// this function for dark mode : -----------------------------------------------------------------------------
 const darkModeBtn = document.querySelector(".darkMode");
 darkModeBtn.addEventListener('click',darkMode);
 let darkMode_Counter = 0;
-console.log();
 
 function darkMode() {
     darkMode_Counter++;
